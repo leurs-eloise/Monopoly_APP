572ca0f4851285a368968dee47b81aa84960afe0
Nico37d <48251652+Nico37d@users.noreply.github.com>
2022-02-09 17:25:35 +0100

    Update Action2.java

--- Monopoly APP/src/Content/Case/Action/Action2.java
+++ Monopoly APP/src/Content/Case/Action/Action2.java
@@ -1,11 +1,25 @@
 package Content.Case.Action;
 
-public class Action2 implements CarteAction{
+public class Action2 implements carteAction{
 
+	public String nom;
+    public int id;
+    
 	@Override
-	public CarteAction doAction() {
+	public carteAction doAction() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	  // Getter & Setter
+    public void setNom (String nom){
+    	this.nom = nom;
+    }
+    
+    public int getId() {
+		return id;
+	}
+    
+    // voir Action1
 
 }
