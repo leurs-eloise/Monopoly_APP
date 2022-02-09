48ecacfeae8d9dead898d4b5b76e63ebbdaa9422
Clem <clemencedessoliers@outlook.fr>
2022-02-09 14:18:43 +0100

    Gare/Terrain

--- Monopoly APP/src/Content/Case/Propriete.java
+++ Monopoly APP/src/Content/Case/Propriete.java
@@ -2,15 +2,17 @@ package Content.Case;
 
 import Content.Joueur;
 
-public class Propriété implements Case {
+public class Propriete implements Case {
 	private int prix;
 	private Joueur joueur;
 	
-	public void propriete(int prix, Joueur joueur) {
+	// Constructeur
+	public void Propriete(int prix, Joueur joueur) {
 		this.prix = prix;
 		this.joueur = joueur;
 	}
-
+	
+	// Getter & Setter
 	public int getPrix() {
 		return prix;
 	}
