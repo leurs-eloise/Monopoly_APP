41061a0f12b0b6d1587192ee681188123ba7ba77
leurs-eloise <efml74@gmail.com>
2022-02-09 17:18:21 +0100

    buildings + loyer terrain

--- Monopoly APP/src/Content/Case/Terrain.java
+++ Monopoly APP/src/Content/Case/Terrain.java
@@ -2,31 +2,53 @@ package Content.Case;
 
 import java.util.ArrayList;
 
+import Content.Configuration;
+import Content.Joueur;
+
 public class Terrain extends Propriete  {
-	private int building;
-	private ArrayList<Integer> loyer = new ArrayList<>();
+	private int nbBuildings;
+	private int loyer;
 	
 	// Constructeur
-	public Terrain(int building, ArrayList<Integer> loyer) {
-		super();
-		this.building = building;
+	public Terrain(int id, int prix, Joueur j, int building, int loyer) {
+		super(id, prix, j);
+		this.nbBuildings = building;
 		this.loyer = loyer;
 	}
 	// Getter & Setter
-	public int getBuilding() {
-		return building;
+	public int getNbBuilding() {
+		return nbBuildings;
 	}
 
-	public void setBuilding(int building) {
-		this.building = building;
+	public void setNbBuilding(int building) {
+		this.nbBuildings = building;
 	}
 
-	public ArrayList<Integer> getLoyer() {
+	public int getLoyer() {
 		return loyer;
 	}
 
-	public void setLoyer(ArrayList<Integer> loyer) {
-		this.loyer = loyer;
+	public void setLoyer() {
+		for(Propriete prop : this.getJoueur().getProprietes()) {
+			if (nbBuildings == 0) {
+				this.loyer = Configuration.getInstance().getLoyer(prop.getId())[0];
+		}
+			if (nbBuildings == 1) {
+				this.loyer = Configuration.getInstance().getLoyer(prop.getId())[1];
+		}
+			if (nbBuildings == 2) {
+				this.loyer = Configuration.getInstance().getLoyer(prop.getId())[2];
+		}
+			if (nbBuildings == 3) {
+				this.loyer = Configuration.getInstance().getLoyer(prop.getId())[3];
+		}
+			if (nbBuildings == 4) {
+				this.loyer = Configuration.getInstance().getLoyer(prop.getId())[4];
+		}
+			if (nbBuildings == 5) {
+				this.loyer = Configuration.getInstance().getLoyer(prop.getId())[5];
+		}
+		
 	}
 	
 	
