55b198bb5186cdfbcad48db4ba4d8390ee623d2a
Nico37d <48251652+Nico37d@users.noreply.github.com>
2022-02-09 17:25:21 +0100

    Update Action1.java

--- Monopoly APP/src/Content/Case/Action/Action1.java
+++ Monopoly APP/src/Content/Case/Action/Action1.java
@@ -1,11 +1,41 @@
 package Content.Case.Action;
 
-public class Action1 implements CarteAction{
+import Content.Joueur;
 
+public class Action1 implements carteAction{
+
+	public String nom;
+    public int id;
+    private Joueur joueur; 
+    
 	@Override
-	public CarteAction doAction() {
+	public carteAction doAction() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	
+	  // Getter & Setter
+    public void setNom (String nom){
+    	this.nom = nom;
+    }
+    
+    public int getId() {
+		return id;
+	}
+    
+    public void Actions(Joueur joueur){
+		cartes=new String[7];
+		cartes[0]= joueur.setPosition(joueur.getPosition()-3); //Reculer de 3 cases 
+		cartes[1]= joueur.setPosition(0); //Avancer case départ
+		cartes[2]= joueur.setPosition(6); //Aller en prison
+		cartes[3]= joueur.setPosition(20); //Rendez-vous Annecy
+		cartes[4]= joueur.setArgent(joueur.getArgent()-15); // Amende pour excès de vitesse : payez 15$
+		cartes[5]= joueur.setArgent(joueur.getArgent()+50); //La banque vous verse 50$
+		cartes[6]= joueur.setArgent(joueur.getArgent()-50); // Frais, payez 50$ 
+	}
+    
+    
 }
+joueur.setPosition(joueur.getPosition()-3);
+	
