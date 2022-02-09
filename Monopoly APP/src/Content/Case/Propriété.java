6a68338a7eb5d0d9ab22a5c78e08eea8533e482b
Clem <clemencedessoliers@outlook.fr>
2022-02-09 13:52:49 +0100

    Popriété

--- Monopoly APP/src/Content/Case/Propriété.java
+++ Monopoly APP/src/Content/Case/Propriété.java
@@ -1,6 +1,32 @@
 package Content.Case;
 
-public class Propriété implements Case {
+import Content.Joueur;
+
+public class PropriÃ©tÃ© implements Case {
+	private int prix;
+	private Joueur joueur;
+	
+	public void propriete(int prix, Joueur joueur) {
+		this.prix = prix;
+		this.joueur = joueur;
+	}
+
+	public int getPrix() {
+		return prix;
+	}
+
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+
+	public Joueur getJoueur() {
+		return joueur;
+	}
+
+	public void setJoueur(Joueur joueur) {
+		this.joueur = joueur;
+	}
+	
 
 
 }
