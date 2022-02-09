8fecf01a9f5b5b8659b1bbabc10c777317f7e1fe
Nico37d <48251652+Nico37d@users.noreply.github.com>
2022-02-09 17:26:32 +0100

    Update CaseCarte.java

--- Monopoly APP/src/Content/Case/CaseCarte.java
+++ Monopoly APP/src/Content/Case/CaseCarte.java
@@ -1,8 +1,30 @@
 package Content.Case;
-
+// Enlever id, nom et action
 public class CaseCarte implements Case {
-  
-    public String nom;
+	
+	public String nom;
     public int id;
     public String action;
+    
+    // Constructeur
+    public CaseCarte(String nom, int id, String action) {
+        this.nom = nom;
+        this.id = id;
+        this.action = action;
+    }
+    
+    // Getter & Setter
+    public void setNom (String nom){
+    	this.nom = nom;
+    }
+    
+    public int getId() {
+		return id;
+	}
+    
+    public String getAction() {
+		return action;
+	}
+
+    
 }
