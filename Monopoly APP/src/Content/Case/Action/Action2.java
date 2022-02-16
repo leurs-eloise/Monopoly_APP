a0c67975e3a1d4377ef0fade33b0ff40232736ca
Nico37d <48251652+Nico37d@users.noreply.github.com>
2022-02-16 16:41:54 +0100

    Update Action2.java

--- Monopoly APP/src/Content/Case/Action/Action2.java
+++ Monopoly APP/src/Content/Case/Action/Action2.java
@@ -1,12 +1,20 @@
 package Content.Case.Action;
 
+import Content.Joueur;
+
 public class Action2 implements carteAction{
 
 	public String nom;
+	private String action;
     public int id;
-
+    private Joueur joueur =  Partie.getInstance().getCurrentPlayer(); 
+    
+    
+    // Constructeur
+	public Action1(String nom, String description) {
+	}
 	
-	  // Getter & Setter
+	// Getter & Setter
     public void setNom (String nom){
     	this.nom = nom;
     }
@@ -15,12 +23,13 @@ public class Action2 implements carteAction{
 		return id;
 	}
 
+
 	@Override
 	public void doAction() {
-		
+		joueur.setPosition(0); //Avancer case depart
 		
 	}
     
-    // voir Action1
-
+  
 }
+	
