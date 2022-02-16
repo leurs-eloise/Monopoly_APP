84495424d0bb32d3e157e781195b8240dff9cb98
Nico37d <48251652+Nico37d@users.noreply.github.com>
2022-02-16 16:47:30 +0100

    Update Action3.java

--- Monopoly APP/src/Content/Case/Action/Action3.java
+++ Monopoly APP/src/Content/Case/Action/Action3.java
@@ -1,1 +1,35 @@
+package Content.Case.Action;
 
+import Content.Joueur;
+
+public class Action1 implements carteAction{
+
+	public String nom;
+	private String action;
+    public int id;
+    private Joueur joueur =  Partie.getInstance().getCurrentPlayer(); 
+    
+    
+    // Constructeur
+	public Action1(String nom, String description) {
+	}
+	
+	// Getter & Setter
+    public void setNom (String nom){
+    	this.nom = nom;
+    }
+    
+    public int getId() {
+		return id;
+	}
+
+
+	@Override
+	public void doAction() {
+		joueur.setPosition(6); //Aller en prison
+		
+	}
+    
+  
+}
+	
