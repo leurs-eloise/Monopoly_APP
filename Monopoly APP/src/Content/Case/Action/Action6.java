346085f518c398a645a74a342ebb043430144d86
Nico37d <48251652+Nico37d@users.noreply.github.com>
2022-02-16 16:49:47 +0100

    Update Action6.java

--- Monopoly APP/src/Content/Case/Action/Action6.java
+++ Monopoly APP/src/Content/Case/Action/Action6.java
@@ -26,7 +26,7 @@ public class Action1 implements carteAction{
 
 	@Override
 	public void doAction() {
-		joueur.setPosition(6); //Aller en prison
+		joueur.setArgent(joueur.getArgent()+50); //La banque vous verse 50$
 		
 	}
     
